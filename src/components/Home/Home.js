f663eaeab73b9617720ebcef6a00706d6520c079
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-02-04 13:49:34 -0500

    Makes updates to the nav

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,14 @@
 import "./Home.css";
 
 function Home() {
-  return <div className="home">Taylor Williams</div>;
+  return (
+    <div className="home">
+      <h1>Taylor Williams</h1>
+
+      <h3>Software Engineer</h3>
+      <h2>Full Stack Developer</h2>
+    </div>
+  );
 }
 
 export default Home;
