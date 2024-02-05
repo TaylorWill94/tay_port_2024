73b91ffdd6e68df64851e3345feb8d5ff883089c
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-02-04 23:11:22 -0500

    Adds expertise information

--- src/components/Expertise/Expertise.js
+++ src/components/Expertise/Expertise.js
@@ -5,14 +5,17 @@ function Expertise() {
     <div className="expertise">
       <div className="sec">
         <h3>Software Developer</h3>
+        <p>Skilled in JavaSCript, React, TypeScript, CSS, and Node.js.</p>
       </div>
 
       <div className="sec">
         <h3>Frontend Developer</h3>
+        <p>Experienced in HTML and CSS.</p>
       </div>
 
       <div className="sec">
         <h3>Backend Developer</h3>
+        <p>Skilled in SQL.</p>
       </div>
     </div>
   );
---
+++ src/components/Folder/Folder.js
@@ -0,0 +1,11 @@
+function Folder() {
+  return (
+    <footer className="footer">
+      <div>
+        <h2>Taylor Williams</h2>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
