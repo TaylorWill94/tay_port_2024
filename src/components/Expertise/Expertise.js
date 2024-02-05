6d9aecef1c59a26d1a089f0fb30da02dfd4c2880
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-02-04 22:48:45 -0500

    Adds expertise

---
+++ src/components/Expertise/Expertise.js
@@ -0,0 +1,21 @@
+import "./Expertise.css";
+
+function Expertise() {
+  return (
+    <div className="expertise">
+      <div className="sec">
+        <h3>Software Developer</h3>
+      </div>
+
+      <div className="sec">
+        <h3>Frontend Developer</h3>
+      </div>
+
+      <div className="sec">
+        <h3>Backend Developer</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Expertise;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,4 @@
+import Expertise from "../Expertise/Expertise";
 import "./Home.css";
 
 function Home() {
@@ -5,8 +6,12 @@ function Home() {
     <div className="home">
       <h1>Taylor Williams</h1>
 
-      <h3>Software Engineer</h3>
-      <h2>Full Stack Developer</h2>
+      <div className="bio">
+        <h3>Software Engineer</h3>
+        <h2>Full Stack Developer & Front End App Developer</h2>
+      </div>
+
+      <Expertise />
     </div>
   );
 }
